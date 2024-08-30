c74c44853a95633d9befdbeafe219d333f9a804a
tjgml1210 <tjgml021210@naver.com>
2024-08-30 16:48:57 +0900

    .

---
+++ 5_JavaScript/resources/js/2.hosting.js
@@ -0,0 +1,19 @@
+/**
+ * Hosting
+ */
+
+console.log(name);
+var name = "seohee";
+console.log(name);
+
+/**
+ * Hosting은 뭘까?
+ * 
+ * 모든 변수 선언문이 코드의 최상단으로 이동되는 것처럼 느껴진는 현상
+ * var와 다르게 let과 const는 tdz를 이용하여
+ * 마치 호이스팅이 일어나지 않은 것처럼 동작하게 해누다.
+ */
+
+console.log(name2);
+let name2 = "seohee";
+console.log(name2);
